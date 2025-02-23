a0e153407ea4d4df5fc37409313d6e06e3c92064
Don <donovanbell@proton.me>
2025-02-23 15:59:25 -0800

    fixed

--- ClovaLink/ClovaLink/src/src/types/pdf-worker.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'pdfjs-dist/build/pdf.worker.min.js' {
-  const workerContent: string;
-  export default workerContent;
-}
-
-declare module '*.worker.js' {
-  const content: string;
-  export default content;
-} 
